6515fe9557edec35569f57231d9fa350a3b9b5a4
enotick-mag <axmadik2001@gmail.com>
2022-11-28 15:33:09 +0500

    header

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
-setTimeout(() => {
+
 let  abc = document.querySelector(".abc")
+
+setTimeout(() => {
 
 
 abc.style.cssText=`
 opacity: 1;
 `
 
-}, 180000);
+}, 1000);
+
+
+function closekglkgf() {
+// let close = document.querySelector(".x")
+
+abc.style.cssText=`
+opacity: 0;
+`
+
+
+}
